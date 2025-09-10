d6257e925b386badcb7b3c752e06c37501487784
Karim Ryde <karye.webb@gmail.com>
2025-09-10 09:02:48 +0200

    Lyssna på knappen med JS

---
+++ kapitel-1/picocss-globetrotter/script.js
@@ -0,0 +1,9 @@
+// Välja vad vi skall lyssna på
+// Välja knappen
+let knapp = document.querySelector('button');
+
+// Lyssna på 'click' på knappen
+knapp.addEventListener('click', function () {
+    console.log("Du klickade på knappen!");
+    
+});
